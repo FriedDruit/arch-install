9cade1769cc2ecd6dd472283288dad9eb3982112
FriedDruit <sebastien.roy23@gmail.com>
2025-01-01 19:58:52 -0400

    setup init config for bspwm

--- post-install.sh
+++ post-install.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 
+USER="solid"
+
 sudo nvim /etc/pacman.conf
 sudo pacman updatedb
 sudo pkgfile --update 
@@ -32,7 +34,7 @@ sudo snapper -c root create -d "**BASE**"
 sudo nvim /etc/snapper/configs/root
 sudo nvim /etc/updatedb.conf 
 
-sudo pacman -S firefox vlc kitty xorg bspwm polybar feh rofi sxhkd xorg-xinit xorg-xrandr brightnessctl ttf-fontawesome
+sudo pacman -S firefox vlc kitty xorg bspwm polybar feh rofi sxhkd xorg-xinit xorg-xrandr brightnessctl ttf-fontawesome stow network-manager-applet
 sudo pacman -S nvidia-open-dkms nvidia-settings nvidia-utils lib32-nvidia-utils cuda opencl-headers opencl-nvidia
 
 sudo nvim /etc/mkinitcpio.conf 
@@ -42,4 +44,17 @@ echo "options nvidia_drm modeset=1 fbdev=1" |sudo tee /etc/modprobe.d/nvidia.con
   
 yay -S nerd-fonts 
 pacman -S blender godot steam
+
+mkdir /home/$USER/.config/{bspwm,sxhkd}
+
+cp /usr/share/doc/bspwm/examples/bspwmrc /home/$USER/.config/bspwmrc
+cp /usr/share/doc/bspwm/examples/sxhkd /home/$USER/.config/sxhkdrc
+
+nvim /home/$USER/.config/sxhkd/sxhkdrc
+nvim /home/$USER/.config/bspwm/bspwmrc
+
+cp /etc/X11/xinit/xinitrc /home/$USER/.xinitrc
+nvim /home/$USER/.xinitrc
+
+
 
